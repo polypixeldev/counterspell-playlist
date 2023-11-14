1f21b12e27f35f093b69b3ac079c33f3e8018350
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-11-13 21:15:41 -0500

    Initial version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
-import Image from 'next/image'
+import Spotify from "spotify-web-api-node";
+
+const spotify = new Spotify();
+spotify.setRefreshToken(process.env.REFRESH_TOKEN!);
+spotify.setClientId(process.env.CLIENT_ID!);
+spotify.setClientSecret(process.env.CLIENT_SECRET!);
 
 export default function Home() {
+  spotify.refreshAccessToken().then((data) => {
+    spotify.setAccessToken(data.body.access_token);
+  });
+
+  async function add(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name");
+    const artist = formData.get("artist");
+
+    const trackRes = await spotify.searchTracks(
+      `track:${name} artist:${artist}`,
+    );
+    const uri = JSON.stringify(trackRes.body.tracks?.items[0].uri);
+    if (process.env.DENY_EXPLICIT === "true") {
+      if (trackRes.body.tracks?.items[0].explicit) {
+        return;
+      }
+    }
+
+    const id = uri.split(/[:\"]+/)[3];
+
+    const addRes = await fetch(
+      `https://api.spotify.com/v1/playlists/${process.env.NEXT_PUBLIC_PLAYLIST_ID}/tracks?uris=spotify%3Atrack%3A${id}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${spotify.getAccessToken()}`,
+        },
+      },
+    );
+
+    console.log(await addRes.json());
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen flex-col items-center justify-start gap-16 bg-green-300 p-4 md:p-10">
+      <h1 className="rounded-2xl border-4 border-black bg-blue-400 p-5 text-center text-4xl font-semibold text-white shadow-black drop-shadow-xl">
+        Gaither Hack Club - Spotify Playlist
+      </h1>
+      <div className="flex flex-col items-center justify-start gap-4 rounded-xl bg-green-400 p-8">
+        <h2 className="text-3xl font-medium">Add a Song</h2>
+        <form
+          action={add}
+          className="flex flex-col items-center justify-between gap-10 md:flex-row md:items-end"
+        >
+          <div className="flex flex-col items-start justify-start">
+            <label className="text-lg">Name</label>
+            <input
+              name="name"
+              type="text"
+              className="flex rounded-md border-2 border-black p-3 text-lg text-black"
+              placeholder="Never Gonna Give You Up"
             />
-          </a>
-        </div>
+          </div>
+          <div className="flex flex-col items-start justify-start">
+            <label className="text-lg">Artist</label>
+            <input
+              name="artist"
+              type="text"
+              className="flex rounded-md border-2 border-black p-3 text-lg text-black"
+              placeholder="Rick Astley"
+            />
+          </div>
+          <button
+            type="submit"
+            className="flex aspect-square items-center rounded-lg bg-green-500 p-4 text-center text-xl font-medium"
+          >
+            Add
+          </button>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <iframe
+        className="h-96 w-full rounded-md md:w-1/2"
+        src={`https://open.spotify.com/embed/playlist/${process.env.NEXT_PUBLIC_PLAYLIST_ID}`}
+        allowFullScreen={false}
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
     </main>
-  )
+  );
 }
